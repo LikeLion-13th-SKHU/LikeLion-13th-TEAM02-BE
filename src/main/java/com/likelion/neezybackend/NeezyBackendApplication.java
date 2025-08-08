a62d1228a5c7c54464e8fc35923ec5539a30e3b1
Junyeong-An <dev.jun0321@gmail.com>
2025-08-08 16:46:47 +0900

    Init: 초기 프로젝트 셋팅

---
+++ src/main/java/com/likelion/neezybackend/NeezyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.neezybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeezyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeezyBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/likelion/neezybackend/NeezyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likelion.neezybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeezyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
