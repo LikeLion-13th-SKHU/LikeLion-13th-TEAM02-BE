5fb06dc76d79775ce68125e712693f5719672aa8
Junyeong-An <dev.jun0321@gmail.com>
2025-08-25 17:52:54 +0900

    [fix] 지역별 게시글 조회 로직 수정

--- src/main/java/com/likelion/neezybackend/post/api/dto/request/PostSaveRequestDto.java
+++ src/main/java/com/likelion/neezybackend/post/api/dto/request/PostSaveRequestDto.java
@@ -1,11 +1,13 @@
 package com.likelion.neezybackend.post.api.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.likelion.neezybackend.post.domain.Category;
 import com.likelion.neezybackend.region.domain.Region;
 
 public record PostSaveRequestDto(
         String title,
         String contents,
+        @JsonProperty("region")
         String regionName,     // 지역 (필수 입력)
         Category category
 ) {
